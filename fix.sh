9adf43b2395a943c6424966618bef1e926ed5fe0
rubin <csrbzhb@126.com>
2026-07-10 16:29:14 +0800

    添加本地打包配置脚本

---
+++ fix.sh
@@ -0,0 +1,18 @@
+export WRT_THEME=aurora
+export WRT_NAME=Jwrt
+export WRT_SSID=Jwrt
+      #默认WIFI密码
+export WRT_WORD=12345678
+      #默认地址
+export WRT_IP=192.168.10.1
+      #默认密码，仅作提示，修改无用
+
+export WRT_MARK="GITHUB_REPOSITORY"
+export WRT_DATE=$(TZ=UTC-8 date +"%y.%m.%d-%H.%M.%S")
+echo $WRT_DATE $WRT_MARK
+cd package/
+sh ../Packages.sh
+sh ../Handles.sh
+cd ..
+sh Settings.sh
+sed -i 's/+python3-pkg-resources //g; s/+python3-email /+python3 /g' package/netspeedtest/luci-app-netspeedtest/Makefile
